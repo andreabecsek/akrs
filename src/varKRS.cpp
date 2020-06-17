b184e6e7d1f3e6403e00014f7aef5461aba21b7f
Douglas Corbin <dc16890@bristol.ac.uk>
2020-06-17 23:50:59 +0100

    Fixed exporting bug.

--- src/varKRS.cpp
+++ src/varKRS.cpp
@@ -1,8 +1,7 @@
 #include <Rcpp.h>
-
-// [[Rcpp::interfaces(cpp)]]
-
 using namespace Rcpp;
+
+// [[Rcpp::interfaces(r, cpp)]]
 
 //'Estimate conditional expectation using a Gaussian Kernel with changing bandwidth.
 //'
@@ -17,40 +16,40 @@ NumericVector varKRS_cpp(const NumericVector y, const NumericVector x, const Num
   int n0 = x0.size();
   NumericVector res(n);
   NumericVector mu(n);
-  
+
   NumericVector madHat(n0);
   NumericVector out(n0);
-  
+
   for(int i = 0; i < n; i++){
     NumericVector kernel = dnorm(x, x[i],lambda,0);
-    
+
     double num = 0;
     for(int j = 0; j < n; j++){
       num = num + kernel[j] * y[j];
     }
-    
+
     double denom = 0;
     for(int j = 0; j < n; j++){
       denom = denom + kernel[j];
     }
     mu[i] = num / denom;
   }
-  
+
   NumericVector resAbs(n);
   resAbs = abs(y - mu);
-  
-  
+
+
   NumericVector w(n0);
   double sum_w = 0;
-  
+
   for(int i=0; i < n0; i++){
     NumericVector kernel = dnorm(x, x0[i],lambda,0);
-    
+
     double num = 0;
     for(int j = 0; j < n; j++){
       num = num + kernel[j] * resAbs[j];
     }
-    
+
     double denom = 0;
     for(int j = 0; j < n; j++){
       denom = denom + kernel[j];
@@ -58,28 +57,28 @@ NumericVector varKRS_cpp(const NumericVector y, const NumericVector x, const Num
     madHat[i] = num / denom;
     w[i] = denom / num;
     sum_w = sum_w + w[i];
-    
+
   }
-  
+
   for(int i = 0; i < n0; i++){
     w[i] = n0 * w[i] / sum_w;
   }
-  
+
   for(int i=0; i < n0; i++){
     NumericVector kernel = dnorm(x, x0[i],lambda*w[i],0);
-    
+
     double num = 0;
     for(int j = 0; j < n; j++){
       num = num + kernel[j] * y[j];
     }
-    
+
     double denom = 0;
     for(int j = 0; j < n; j++){
       denom = denom + kernel[j];
     }
     out[i] = num / denom;
   }
-  
-  
+
+
   return out;
 }
